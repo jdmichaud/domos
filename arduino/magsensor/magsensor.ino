0c8cecfc5605713f0519fe8a9eba0bee2a18fe0c
JD <jean.daniel.michaud@gmail.com>
2016-04-19 19:16:40 +0200

    Add sending of packet to magsensor project

--- arduino/magsensor/magsensor.ino
+++ arduino/magsensor/magsensor.ino
@@ -1,9 +1,11 @@
 #include <EEPROM.h>
 #include <RCSwitch.h>
 #include <LowPower.h>
+#include "limits.h"
 #include "protocol.h"
+#include "sensor_types.h"
 
-//RCSwitch rfDevice = RCSwitch();
+RCSwitch rfDevice = RCSwitch();
 
 int SERIAL_COMMUNICATION_SPEED = 9600;
 // How long do we sleep. The longer, the better for the battery but the
@@ -13,13 +15,47 @@ period_t SLEEP_PERIOD = SLEEP_FOREVER;
 // Pin on which the magnetic swith is plugged.
 int MAGNETIC_SWITCH_PIN = 2;
 // Pin on which the RF device is plugged;
-int RF_DEVICE_PIN = 13;
+int RF_DEVICE_COMM_PIN = 13;
+int RF_DEVICE_POWER_PIN = 10;
 // Status of the magnetic switch. We consider the door closed
 boolean g_contact = true;
 // EEPROM address usage
 int EE_DEVICE_NUMBER = 0;
 // device number
 int g_device_number;
+
+void setupRFDevice() {
+  // Transmitter is connected to Arduino Pin #10
+  rfDevice.enableTransmit(RF_DEVICE_COMM_PIN);
+  // Optional set pulse length.
+  // rfDevice.setPulseLength(320);
+  // Optional set protocol (default is 1, will work for most outlets)
+  // rfDevice.setProtocol(2);
+  // Optional set number of transmission repetitions.
+  // rfDevice.setRepeatTransmit(15);
+}
+
+void sendStatus(boolean open) {
+  char codeWord[65];
+  packet_t packet;
+  // Power up RF device
+  digitalWrite(RF_DEVICE_POWER_PIN, HIGH);
+  setupRFDevice();
+  // Create the communication packet
+  uint8_t message = open ? 1 : 0;
+  create_packet(DOOR_SENSOR, g_device_number, 1, &message, &packet, NULL);
+  codeWord[64] = '\0';
+  for (int packetSize = sizeof (packet_t) * CHAR_BIT; packetSize; --packetSize) {
+    codeWord[packetSize] = packet & 1 ? '1' : '0';
+    packet >>= 1;
+  }
+  Serial.print("sending ");
+  Serial.println(codeWord);
+  // Send packet
+  rfDevice.send(codeWord);
+  // Power down RF device
+  digitalWrite(RF_DEVICE_POWER_PIN, LOW);
+}
 
 // Function called on door state change
 void doorStateChange() {
@@ -28,32 +64,14 @@ void doorStateChange() {
   // Does the contact status changed since we last check
   if (newContact != g_contact) {
     // Send the status of the contact on one bit
-    //rfDevice.send(contact, 1);
-    flashLED();
+    sendStatus(newContact);
     // Update the status of the contact
     g_contact = newContact;
-    //Serial.print("pushing to RF device: ");
-    //Serial.println(contact);
   }
 }
 
-void flashLED() {
-  digitalWrite(RF_DEVICE_PIN, HIGH);
-  delay(50);
-  digitalWrite(RF_DEVICE_PIN, LOW);
-}
-
 void setup() {
-  /***********************  RF Device initialisation **************************/
   Serial.begin(SERIAL_COMMUNICATION_SPEED);
-  // Transmitter is connected to Arduino Pin #10
-  //rfDevice.enableTransmit(RF_DEVICE_PIN);
-  // Optional set pulse length.
-  // rfDevice.setPulseLength(320);
-  // Optional set protocol (default is 1, will work for most outlets)
-  // rfDevice.setProtocol(2);
-  // Optional set number of transmission repetitions.
-  // rfDevice.setRepeatTransmit(15);
   /*****************************  Configuration *******************************/
   g_device_number = EEPROM.read(EE_DEVICE_NUMBER);
   if (!g_device_number) { // If not setup already, configure it to default value 1
@@ -62,7 +80,8 @@ void setup() {
   }
   /***************************  Pin initialisation ****************************/
   pinMode(MAGNETIC_SWITCH_PIN, INPUT);
-  pinMode(RF_DEVICE_PIN, OUTPUT);
+  pinMode(RF_DEVICE_COMM_PIN, OUTPUT);
+  pinMode(RF_DEVICE_POWER_PIN, OUTPUT);
   /*********************** Interruption initialisation ************************/
   // Allow wake up pin to trigger interrupt on state change.
   attachInterrupt(digitalPinToInterrupt(MAGNETIC_SWITCH_PIN), doorStateChange, CHANGE);
---
+++ lib/include/sensor_types.h
@@ -0,0 +1,6 @@
+#ifndef __SENSOR_TYPE_H__
+#define __SENSOR_TYPE_H__
+
+#define DOOR_SENSOR 5
+
+#endif //  __SENSOR_TYPE_H__
