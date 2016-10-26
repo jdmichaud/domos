7867ee7b61a4684d098e4f41df16ba24e577dae5
JD <jeandaniel.michaud@ge.com>
2016-10-26 17:33:06 +0200

    Remove useless folder.

--- rpi/receiver/rfprotocol/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jedi/domos/lib/rfprotocol/test
-# Build directory: /home/jedi/domos/rpi/receiver/rfprotocol/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
