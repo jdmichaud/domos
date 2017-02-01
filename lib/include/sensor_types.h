b80862779af5a4d465108304bf4a72a57bd13a7d
jdmichaud <jdmichaud@users.noreply.github.com>
2017-02-01 10:53:31 +0100

    Add sensor types

--- lib/include/sensor_types.h
+++ lib/include/sensor_types.h
@@ -1,6 +1,12 @@
 #ifndef __SENSOR_TYPE_H__
 #define __SENSOR_TYPE_H__
 
-#define DOOR_SENSOR 5
+#define DOOR_SENSOR     5
+#define THERMOMETER     6
+#define HUMIDITY_SENSOR 7
+#define MOTION_SENSOR   8
+#define CAMERA          9
+#define LOCK            10
+#define KEYPAD          11
 
 #endif //  __SENSOR_TYPE_H__
