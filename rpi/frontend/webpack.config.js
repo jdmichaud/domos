7cd542dcbb780ebd59e67ccfa3440317403846ce
JD <jeandaniel.michaud@ge.com>
2017-03-08 14:32:01 +0100

    Add bootstrap and jquery.

--- rpi/frontend/webpack.config.js
+++ rpi/frontend/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: path.join('app', 'js', 'main.ts'),
@@ -9,7 +11,7 @@ module.exports = {
   devtool: "source-map",
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".ts", ".js"],
+    extensions: [".ts", ".js", ],
     modules: [
       "node_modules",
       path.resolve(__dirname, ".")
@@ -27,7 +29,26 @@ module.exports = {
         test: /\.tsx?$/, 
         use: 'awesome-typescript-loader?configFileName="tscondig.json"',
         exclude: /(node_modules)/,
-      }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+        ],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)([\?]?.*)$/,
+        use: 'file-loader?name=resources/img/[name].[ext]?[hash]',
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot)([\?]?.*)$/,
+        use: 'file-loader?name=resources/fonts/[name].[ext]?[hash]',
+      },
     ],
   },  
+  plugins: [
+    new webpack.ProvidePlugin({ jQuery: "jquery" }),
+    new CopyWebpackPlugin([{ from: 'app/index.html' }]),
+  ],
 };
