df17cefb48c58fa84c1a13ead65fb33d01d6cc8f
jd <jean.daniel.michaud@gmail.com>
2017-04-23 19:18:23 +0200

    post-build to increase videocore memory and only activate uart if not already activated.

--- host/docker/buildroot/board/raspberrypi0/post-build.sh
+++ host/docker/buildroot/board/raspberrypi0/post-build.sh
@@ -10,5 +10,15 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
 
+CONFIG_FILE=${TARGET_DIR}/../images/rpi-firmware/config.txt
+
+# Increase the memory allocated to the VideoCore chip
+sed -i s/gpu_mem_512=100/gpu_mem_512=256/ ${CONFIG_FILE}
+
 # Activate the console
-echo "enable_uart=1" | cat >> ${TARGET_DIR}/../images/rpi-firmware/config.txt
+if grep -q enable_uart=1 ${CONFIG_FILE}
+then
+  echo "uart already configured"
+else
+  echo "enable_uart=1" | cat >> ${CONFIG_FILE}
+fi
