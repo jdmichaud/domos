7e6cda18c089041f2c90e50bd3b6bdacd3863ffa
JD <jean.daniel.michaud@gmail.com>
2017-05-06 18:01:08 +0200

    Point to the proper CMakeLists.txt

--- host/docker/buildroot/package/domos/receiver/receiver.mk
+++ host/docker/buildroot/package/domos/receiver/receiver.mk
@@ -10,6 +10,6 @@ RECEIVER_SITE_METHOD = git
 RECEIVER_LICENSE = Apache v2.0
 RECEIVER_LICENSE_FILES = LICENSE
 RECEIVER_DEPENDENCIES = libhttp rcswitch librfprotocol
-RECEIVER_SUBDIR = rpi/receiver
+RECEIVER_SUBDIR = rpi/receiver/buildroot
 
 $(eval $(cmake-package))
