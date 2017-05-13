78d22d8f7cac256b71bf3a1c82aa183481ce1f55
jd <jean.daniel.michaud@gmail.com>
2017-05-13 22:31:13 +0200

    WiringPi install phase fail when using multiple jobs. Force single job when installing.

--- host/docker/buildroot/prepare_image.sh
+++ host/docker/buildroot/prepare_image.sh
@@ -71,6 +71,22 @@ function controller() {
 endmenu\
 \
 ' ${destination}/package/Config.in
+  # wiringPi has some trouble to build in parallel
+  patch --forward --reject-file=- ${destination}/package/wiringpi/wiringpi.mk << 'EOF'
+--- a/wiringpi.mk       2017-05-13 18:46:12.251577789 +0000
++++ b/wiringpi.mk       2017-05-13 18:46:37.943713759 +0000
+@@ -37,8 +37,8 @@
+ endef
+
+ define WIRINGPI_INSTALL_TARGET_CMDS
+-	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/wiringPi $(WIRINGPI_LIB_INSTALL_TARGETS) DESTDIR=$(TARGET_DIR) PREFIX=/usr LDCONFIG=true
+-	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/devLib $(WIRINGPI_LIB_INSTALL_TARGETS) DESTDIR=$(TARGET_DIR) PREFIX=/usr LDCONFIG=true
++	$(TARGET_MAKE_ENV) $(MAKE1) -C $(@D)/wiringPi $(WIRINGPI_LIB_INSTALL_TARGETS) DESTDIR=$(TARGET_DIR) PREFIX=/usr LDCONFIG=true
++	$(TARGET_MAKE_ENV) $(MAKE1) -C $(@D)/devLib $(WIRINGPI_LIB_INSTALL_TARGETS) DESTDIR=$(TARGET_DIR) PREFIX=/usr LDCONFIG=true
+	$(INSTALL) -D -m 0755 $(@D)/gpio/gpio $(TARGET_DIR)/usr/bin/gpio
+	$(INSTALL) -D -m 0755 $(@D)/gpio/pintest $(TARGET_DIR)/usr/bin/pintest
+ endef
+EOF
   # No board script to be overridden
 }
 
