3400c6ef354179011570fb20b249bcc32e189830
jd <jean.daniel.michaud@gmail.com>
2017-05-14 22:44:10 +0200

    Try fixing kiosk package

--- host/docker/buildroot/package/domos/kiosk/kiosk.mk
+++ host/docker/buildroot/package/domos/kiosk/kiosk.mk
@@ -8,6 +8,6 @@ KIOSK_VERSION = master
 KIOSK_SITE = git://github.com/jdmichaud/kiosk
 KIOSK_SITE_METHOD = git
 KIOSK_LICENSE = MIT
-KIOSK_DEPENDENCIES = qt5base qt5webkit qt5declarative
+KIOSK_DEPENDENCIES = qt5base qt5declarative qt5webengine
 
 $(eval $(cmake-package))
