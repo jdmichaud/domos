d3cfeb7cea59c07366700f73ef1d3215b0091690
JD <jean.daniel.michaud@gmail.com>
2017-05-26 14:49:14 +0200

    Use new one word protocol.

--- rpi/receiver/src/RF33_adapter.cpp
+++ rpi/receiver/src/RF33_adapter.cpp
@@ -29,10 +29,9 @@ int RF33Adapter::init(int pin) {
 
 int RF33Adapter::receiveMessage(std::function<void(const packet_s &)> callback) {
   bool receiving_first = true;
-  uint64_t previous_value = 0;
-  uint64_t value = 0;
+  packet_t value = 0;
+  packet_t previous_value = 0;
   uint64_t count = 0;
-  packet_t raw_packet = 0;
   VERBOSE(std::cout << "listening..." << std::endl);
   while(1) {
     std::unique_lock<std::mutex> lk(m);
@@ -47,27 +46,17 @@ int RF33Adapter::receiveMessage(std::function<void(const packet_s &)> callback) 
       if (value == previous_value)
         continue; // If we receive the same value, this is just a repeat
       previous_value = value;
-      raw_packet |= value;
-      // If we are receiving the first part of the packet, shift the value by
-      // 32 to let room for the second half
-      if (receiving_first) {
-        raw_packet <<= 32;
-        receiving_first = false;
-      } else {
-        // We received the second part, process it
-        packet_s packet;
-        int ret = read_packet(raw_packet, &packet);
-        if (ret == WRONG_MAGIC_ERROR) {
-          std::cout << "DEBUG: Wrong magic number, dismiss message" << std::endl;
-          continue;
-        } else if (ret == PARITY_ERROR) {
-          std::cerr << "ERROR: Parity error in message, tread lightly" << std::endl;
-        }
-        callback(packet);
-        receiving_first = true;
-        raw_packet = 0;
-        value = 0;
+      // Now unpack it
+      packet_s packet;
+      int ret = read_packet(value, &packet);
+      if (ret == WRONG_MAGIC_ERROR) {
+        std::cout << "DEBUG: Wrong magic number, dismiss message" << std::endl;
+        continue;
+      } else if (ret == PARITY_ERROR) {
+        std::cerr << "ERROR: Parity error in message, tread lightly" << std::endl;
       }
+      callback(packet);
+      value = 0;
     }
   }
 
