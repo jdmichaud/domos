ced990d790e7a63d9e7aa1671dd5a46fd4f7118e
JD <jean.daniel.michaud@gmail.com>
2017-05-26 14:08:26 +0200

    As we not limit the number of different device to 16 (2^4), need to start counting from 1 instead of arbitrarily starting from 5.

--- lib/rfprotocol/src/sensor_types.h
+++ lib/rfprotocol/src/sensor_types.h
@@ -1,13 +1,13 @@
 #ifndef __SENSOR_TYPE_H__
 #define __SENSOR_TYPE_H__
 
-#define DOOR_SENSOR     5
-#define THERMOMETER     6
-#define HUMIDITY_SENSOR 7
-#define MOTION_SENSOR   8
-#define CAMERA          9
-#define LOCK            10
-#define KEYPAD          11
+#define DOOR_SENSOR     1
+#define THERMOMETER     2
+#define HUMIDITY_SENSOR 3
+#define MOTION_SENSOR   4
+#define CAMERA          5
+#define LOCK            6
+#define KEYPAD          7
 
 #define GET_RESOURCE_NAME(type) \
   (type == DOOR_SENSOR) ? "door-sensors" : \
