65a9525354f22296bf5493873ea84818fdea6e06
jd <jean.daniel.michaud@gmail.com>
2017-06-13 18:24:47 +0200

    Add debug dwitch to kiosk

--- host/docker/buildroot/package/domos/kiosk/kiosk.mk
+++ host/docker/buildroot/package/domos/kiosk/kiosk.mk
@@ -10,4 +10,6 @@ KIOSK_SITE_METHOD = git
 KIOSK_LICENSE = MIT
 KIOSK_DEPENDENCIES = qt5base qt5declarative qt5webengine qt5webview
 
+KIOSK_CONF_OPTS = -DCMAKE_BUILD_TYPE=debug
+
 $(eval $(cmake-package))
