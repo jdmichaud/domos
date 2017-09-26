9f6bacefa7e98471c861ca0fbdbde4ed704bd262
Jean-Daniel Michaud <jeandaniel.michaud@ge.com>
2017-09-26 15:48:22 +0200

    Add camera component.

---
+++ rpi/frontend/app/js/camera.component.ts
@@ -0,0 +1,15 @@
+/**
+ * Application main component
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'camera',
+  template: `
+    <video></video>
+  `,
+})
+export class CameraComponent {
+  constructor() {
+  }
+}
