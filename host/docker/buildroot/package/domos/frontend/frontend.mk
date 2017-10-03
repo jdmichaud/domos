4174c560dceabc1efa936cdaaa45ee5be854cc70
jd <jean.daniel.michaud@gmail.com>
2017-10-03 19:03:03 +0200

    Fix frontend buildsystem

--- host/docker/buildroot/package/domos/frontend/frontend.mk
+++ host/docker/buildroot/package/domos/frontend/frontend.mk
@@ -11,3 +11,9 @@ FRONTEND_LICENSE = Apache v2.0
 FRONTEND_LICENSE_FILES = LICENSE
 HOST_FRONTEND_DEPENDENCIES = host-python host-nodejs
 FRONTEND_SUBDIR = rpi/frontend
+
+$(eval $(generic-package))
+
+define SET_OVERSCAN_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/dist/* $(TARGET_DIR)/var/www
+endef
