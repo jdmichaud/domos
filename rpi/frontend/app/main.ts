0c9ab219346a7b5d303cd4ddff355dfb073b35ca
jd <jean.daniel.michaud@gmail.com>
2017-11-05 14:02:18 +0100

    Fix import order

--- rpi/frontend/app/main.ts
+++ rpi/frontend/app/main.ts
@@ -2,8 +2,8 @@
  * Main file
  */
 // Those because of some weird error. Angular2 is really not ready for prod...
-import 'zone.js';
 import 'reflect-metadata';
+import 'zone.js';
 
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
